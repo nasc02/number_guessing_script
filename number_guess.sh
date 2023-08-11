47c4d294155e94e26e653127188bffef39dd2e01
nasc02 <henriqueaugusto840@gmail.com>
2023-08-11 01:18:17 +0000

    chore:Third commit

--- number_guess.sh
+++ number_guess.sh
@@ -5,14 +5,14 @@ PSQL="psql --username=freecodecamp --dbname=numberguessinggamedb -t --no-align -
 echo "Enter your username:"
 read USERNAME
 
-USERNAME_DATABASE=$($PSQL "SELECT name FROM users WHERE name = $USERNAME")
+USERNAME_DATABASE=$($PSQL "SELECT name FROM users WHERE name = '$USERNAME'")
 
 if [[ -z $USERNAME_DATABASE ]]
 then
  echo "Welcome, $USERNAME! It looks like this is your first time here."
- $($PSQL "INSERT INTO users(name) VALUES($USERNAME)")
+ INSERT_USERNAME=$($PSQL "INSERT INTO users(name) VALUES('$USERNAME')")
 else
-  Welcome back, $USERNAME_DATABASE! You have played <games_played> games, and your best game took <best_game> guesses. #TODO
+  echo "Welcome back, $USERNAME_DATABASE! You have played <games_played> games, and your best game took <best_game> guesses." #TODO
 fi
 
 random_number=$(shuf -i 1-1000 -n 1)
