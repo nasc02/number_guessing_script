c597d742ceb07e7229502885909b39f3c1bdb5d9
nasc02 <henriqueaugusto840@gmail.com>
2023-08-11 15:18:01 +0000

    chore:Fifth commit

--- number_guess.sh
+++ number_guess.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=numberguessinggamedb -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-echo "Enter your username:"
+echo -e "Enter your username:" 
 read USERNAME
 
 USERNAME_DATABASE=$($PSQL "SELECT name FROM users WHERE name = '$USERNAME'")
 
-if [[ -z $USERNAME_DATABASE ]]
-then
- echo "Welcome, $USERNAME! It looks like this is your first time here."
- INSERT_USERNAME=$($PSQL "INSERT INTO users(name) VALUES('$USERNAME')")
+if [[ -z $USERNAME_DATABASE ]]; then
+  if [[ -n "$USERNAME" && ! "$USERNAME" =~ ^[[:space:]]+$ ]]; then
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+    INSERT_USERNAME=$($PSQL "INSERT INTO users(name) VALUES('$USERNAME')")
+  else
+    echo "Invalid username."
+    exit 0
+  fi
 else
   echo "Welcome back, $USERNAME_DATABASE! You have played <games_played> games, and your best game took <best_game> guesses." #TODO
 fi
 
-random_number=$(shuf -i 1-1000 -n 1)
+RANDOM_NUMBER=$(shuf -i 1-1000 -n 1)
 
 GUESS_ATTEMPT(){
 echo "Guess the secret number between 1 and 1000:"
 read INPUT
 }
 
-NUMBER_OF_GUESSES=0
+echo $RANDOM_NUMBER #test
+
+NUMBER_OF_GUESSES=1
 GUESS_ATTEMPT
-if [[ $INPUT < $random_number ]]
-then
+while true; do
+if [[ $INPUT -gt $RANDOM_NUMBER ]]; then
   echo -e "\nIt's lower than that, guess again:\n"
-  $NUMBER_OF_GUESSES+=1
+  NUMBER_OF_GUESSES=$(($NUMBER_OF_GUESSES + 1))
   GUESS_ATTEMPT
-else if [[ $INPUT > $random_number ]]
+elif [[ $INPUT -lt $RANDOM_NUMBER ]]; then
   echo -e "\nIt's higher than that, guess again:\n"
-  $NUMBER_OF_GUESSES+=1
+  NUMBER_OF_GUESSES=$(($NUMBER_OF_GUESSES + 1))
   GUESS_ATTEMPT
-else if ! [[ "$INPUT" =~ ^[0-9]+$ ]]
+elif ! [[ "$INPUT" =~ ^[0-9]+$ ]]; then
   echo -e "\nThat is not an integer, guess again:\n"
-  $NUMBER_OF_GUESSES+=1
+  NUMBER_OF_GUESSES=$(($NUMBER_OF_GUESSES + 1))
   GUESS_ATTEMPT
 else
-  echo -e "\nYou guessed it in $NUMBER_OF_GUESSES tries. The secret number was $random_number. Nice job!\n"
+  echo -e "\nYou guessed it in $NUMBER_OF_GUESSES tries. The secret number was $RANDOM_NUMBER. Nice job!\n"
+  break
 fi
+done
